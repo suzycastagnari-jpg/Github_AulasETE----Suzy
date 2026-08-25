86f3e4f00322fcf37f82f700aa5c0f0809d8eb67
Suzy <suzelayne.2521190431024@aluno.eteprc.faetec.rj.gov.br>
2026-08-25 17:19:33 -0300

    first commit

--- subir.cmd
+++ subir.cmd
@@ -5,3 +5,4 @@ git commit -m "first commit"
 git branch -M main
 ::git remote add origin git@github.com:suzycastagnari-jpg/Aulagithub_20-08---Suzy.git
 git push -u origin main
+start https://github.com/suzycastagnari-jpg/Aulagithub_20-08---Suzy/tree/main/aula6
