d7dadeaa76319baf0626374be7368d07cf2ad8a1
Suzy <suzelayne.2521190431024@aluno.eteprc.faetec.rj.gov.br>
2026-08-27 13:43:23 -0300

    first commit

---
+++ aula3/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'projeto/mvc',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8ef22255235aaab3dff031fa130bde80f1922fe2',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'projeto/mvc' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8ef22255235aaab3dff031fa130bde80f1922fe2',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
